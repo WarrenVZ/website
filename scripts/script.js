566409566858361931b4b1f0cbd879459f93cab6
WarrenVZ <warrenvz@protonmail.com>
2025-05-10 14:45:19 +0200

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-if(hamburger && navLinks){
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-}
